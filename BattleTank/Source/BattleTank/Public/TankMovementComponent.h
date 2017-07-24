c8d47eb90bc04482916802626fc4a58689dff365
Lenovo\Computer <tiberiu_andrei99@yahoo.com>
2017-07-24 15:29:43 +0300

    BT57 A Better Component Architecture

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -7,9 +7,10 @@
 #include "TankMovementComponent.generated.h"
 
 /**
- * 
+ * Responsible for driving the tank tracks
  */
 class ATank;
+class UTankTrack;
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
@@ -17,6 +18,13 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 };
