daebc97fd8159ad50c02abc44c8dbfaabbfbec11
Lenovo\Computer <tiberiu_andrei99@yahoo.com>
2017-07-24 17:10:04 +0300

    BT60 Dissecting RequestDirectMove()

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -25,3 +25,11 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 	// TODO prevent double-speed to dual control use
 }
 
+void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
+{
+	// No need to call Super as we're replacing the functionality
+	auto TankName = GetOwner()->GetName();
+	auto MoveVelocityString = MoveVelocity.ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to  %s"), *TankName, *MoveVelocityString);
+}
+
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -20,4 +20,8 @@ public:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	// How close can the AI tank get 
+	float AcceptanceRadius = 3000;
 };
