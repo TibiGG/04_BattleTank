ea70f4f83556045d0196c93efba733df627aa134
Lenovo\Computer <tiberiu_andrei99@yahoo.com>
2017-07-30 19:23:37 +0300

    BT93 Using TakeDamage() on Actors

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -49,8 +49,18 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 	SetRootComponent(ImpactBlast);
 	CollisionMesh->DestroyComponent();
 
+	UGameplayStatics::ApplyRadialDamage(
+		this,
+		ProjectileDamage,
+		GetActorLocation(),
+		ExplosionForce->Radius, // for consistency
+		UDamageType::StaticClass(),
+		TArray<AActor*>() // damage all actors
+	);
+
 	FTimerHandle Timer;
 	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
+	//TODO Check which one is better between GetTimerManager and SetLifeSpan()
 }
 
 void AProjectile::OnTimerExpire()
